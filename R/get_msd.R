9f891ea149b869e9636f4228cdf74d16167188de
James Curley <curleyjp0@gmail.com>
2021-04-10 16:03:47 -0500

    first commit

---
+++ R/get_msd.R
@@ -0,0 +1,14 @@
+#' Get data with specific mean and standard deviation
+#'
+#' @param n  Number of observations
+#' @param mean Specified mean
+#' @param sd Specified standard deviation
+#' @return A vector of size n between 0 and 1
+#' @examples
+#' get_msd(n=100,mean=4,sd=1)
+#' @importFrom stats "rnorm"
+#' @export
+
+get_msd <- function(n = NULL, mean = NULL, sd = NULL){
+mean+sd*scale(stats::rnorm(n))
+}
